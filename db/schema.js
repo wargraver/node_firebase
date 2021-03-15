fea51d4f4be58f461e7f89a21b66bc5428261495
wargraver <utkarshkrsingh2091@gmail.com>
2021-03-16 02:04:07 +0530

    added schemas

---
+++ db/schema.js
@@ -0,0 +1,11 @@
+class User{
+    constructor(id,firstname,lastname,age,gender,phone_no){
+        this.id=id,
+        this.firstname=firstname,
+        this.lastname=lastname,
+        this.age=age,
+        this.gender=gender,
+        this.phone_no=phone_no
+    }
+}
+module.exports = {User}
