06bb0a837a66bcc9219105c640d163dda10d365c
wargraver <utkarshkrsingh2091@gmail.com>
2021-03-16 03:04:55 +0530

    routes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env' });
-const app =express();
+const app =express()
 const port=process.env.PORT
+const db=require('./db/db_connect.js')
 
 
 //using middlewares to handle requestes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/crud',require('./routes/crud.js'))
 
 
 //starting the server up
--- routes/crud.js
+++ routes/crud.js
@@ -1,3 +1,40 @@
 const express = require('express')
 const app = express()
 const route=express.Router()
+const User=require('../db/schema.js').User
+const firebase= require('../db/db_connect.js')
+const firestore = firebase.firestore();
+
+
+//create a user
+route.post('/user',async (req,res,next)=>{
+   try{ 
+    const data = req.body;
+    await firestore.collection('users').doc().set(data);
+    res.send('user created successfully');
+} catch (error) {
+    res.status(400).send(error);
+}
+})
+
+
+
+//get a specific user
+route.get('/user/:firstname',async (req, res, next) => {
+    try {
+        const firstname = req.params.firstname;
+        const user = await firestore.collection('users').doc(firstname);
+        const data = await user.get();
+        if(!data.exists) {
+            res.status(404).send('User with the given ID not found');
+        }else {
+            res.send(data.data());
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
+
+
+
+module.exports=route
